43920a35744823b00f05b8ab04a0575b47eae9b4
LeoDiazL <leo@bitovi.com>
2023-12-14 16:57:44 -0300

    Fix

--- scripts/create_tf_state_bucket.sh
+++ scripts/create_tf_state_bucket.sh
@@ -11,6 +11,21 @@ else
   else
     aws s3api create-bucket --bucket $TF_STATE_BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION --acl private|| true
   fi
+  
+  AWS_PRINCIPAL=$(aws sts get-caller-identity --query "Arn" --output text)
+
+  # Set bucket policy
+  aws s3api put-bucket-policy --bucket "$TF_STATE_BUCKET" --policy '{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "'"$AWS_PRINCIPAL"'",
+        "Action": "s3:*",
+        "Resource": "arn:aws:s3:::'"$TF_STATE_BUCKET"'/*"
+      }
+    ]
+  }'
 
   if ! [[ -z $(aws s3api head-bucket --bucket $TF_STATE_BUCKET 2>&1) ]]; then
     echo "Bucket does not exist or permission is not there to use it."
